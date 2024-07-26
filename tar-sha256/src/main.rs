73df6931c5a9a1033dffd02e6ffb583ba61baec3
secext2022 <secext2022@outlook.com>
2024-07-27 04:36:59 +0800

    添加(tar-sha256): 使用 pm-bin

--- tar-sha256/src/main.rs
+++ tar-sha256/src/main.rs
@@ -1,31 +1,24 @@
 #![deny(unsafe_code)]
 
-use std::env;
 use std::process::ExitCode;
 
-use env_logger;
+use pm_bin::{cli_arg, init_env_logger, pm_init};
+pm_init!();
 
-mod bin;
 mod calc;
 
 fn main() -> Result<(), ExitCode> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    init_env_logger();
 
-    // 命令行参数处理
-    match env::args().skip(1).next() {
-        Some(i) => match i.as_str() {
-            "--version" | "--版本" => {
-                bin::版本();
-                Ok(())
-            }
-            _ => {
-                calc::tar_sha256(i.into()).unwrap();
-                Ok(())
-            }
-        },
-        _ => {
+    if let Some(a) = cli_arg(print_version) {
+        if a.len() != 1 {
             println!("bad CLI");
             Err(ExitCode::from(1))
+        } else {
+            calc::tar_sha256(a[0].clone()).unwrap();
+            Ok(())
         }
+    } else {
+        Ok(())
     }
 }
