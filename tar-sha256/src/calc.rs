9a6d9c0194c5b7f882c93d3dceb8f98b7670452d
secext2022 <secext2022@outlook.com>
2024-07-29 14:56:58 +0800

    添加(tar-sha256): cat 1.tar | ./tar-sha256 -

--- tar-sha256/src/calc.rs
+++ tar-sha256/src/calc.rs
@@ -1,13 +1,31 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{self, stdin, Read, Stdin};
 
 use sha2::{Digest, Sha256};
 use tar::{Archive, EntryType};
 
+enum Read1 {
+    F(File),
+    S(Stdin),
+}
+
+impl Read for Read1 {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        match self {
+            Self::F(r) => r.read(buf),
+            Self::S(r) => r.read(buf),
+        }
+    }
+}
+
 pub fn tar_sha256(filename: String) -> Result<(), Box<dyn Error>> {
-    let f = File::open(filename)?;
-    let mut a = Archive::new(f);
+    // `-` -> stdin
+    let r = match filename.as_str() {
+        "-" => Read1::S(stdin()),
+        _ => Read1::F(File::open(filename)?),
+    };
+    let mut a = Archive::new(r);
 
     for i in a.entries()? {
         let mut f = i?;
